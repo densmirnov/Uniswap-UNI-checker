84d724dae2bd95ee6b6df5d88e3217c38aecd321
Denis Smirnov <densmirnov@me.com>
2020-09-17 13:55:45 +0300

    Initial commit

---
+++ uni_checker.sh
@@ -0,0 +1,12 @@
+#!./usr/local/bin/bash
+
+# Uniswap $UNI checker. Provide a file with list of your accounts and run the script against it. You will get list of acoounts valid for claim in result.txt
+filename="$1"
+while read -r line
+do
+    name="$line"
+	result=$(curl -# -s -k -X GET https://gentle-frost-9e74.uniswap.workers.dev/1/$name)
+    echo -e "\n account: $name $result"
+	echo -e " " >>result.txt
+done < "$filename"
+# done
